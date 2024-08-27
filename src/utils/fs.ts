ff834606faed17d266af40f55e82ec0d8431298a
滑稽world <65672655+huajiworld@users.noreply.github.com>
2024-08-27 20:10:57 +0800

    Update fs.ts

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -63,21 +63,22 @@ export const downloadFile = (url: string, path: string, options: Omit<RNFS.Downl
     }
   }
   return RNFS.downloadFile({
-    fromUrl: url, // URL to download file from
-    toFile: path, // Local filesystem path to save the file to...options,
-    headers: options.headers, // An object of headers to be passed to the server
-    background?: boolean,    // Continue the download in the background after the app terminates (iOS only)
-    discretionary?: boolean,  // Allow the OS to control the timing and speed of the download to improve perceived performance  (iOS only)
-    cacheable?: boolean,      // Whether the download can be stored in the shared NSURLCache (iOS only, defaults to true)
-    progressInterval: options.progressInterval,
-    progressDivider: options.progressDivider,
-    begin: (res: DownloadBeginCallbackResult) => void,
-    progress?: (res: DownloadProgressCallbackResult) => void,
-    resumable?: () => void,    // only supported on iOS yet
-    connectionTimeout?: number, // only supported on Android yet
-    readTimeout?: number,    // supported on Android and iOS
-    backgroundTimeout?: number,// Maximum time (in milliseconds) to download an entire resource (iOS only, useful for timing out background downloads)
-  })
+  fromUrl: url, // URL to download file from
+  toFile: path, // Local filesystem path to save the file to
+  headers: options.headers, // An object of headers to be passed to the server
+  background: options.background,    // Continue the download in the background after the app terminates (iOS only)
+  discretionary: options.discretionary,  // Allow the OS to control the timing and speed of the download to improve perceived performance  (iOS only)
+  cacheable: options.cacheable,      // Whether the download can be stored in the shared NSURLCache (iOS only, defaults to true)
+  progressInterval: options.progressInterval,
+  progressDivider: options.progressDivider,
+  begin: (res: DownloadBeginCallbackResult) => void,
+  progress: options.progress, // Callback for download progress
+  resumable: options.resumable,    // only supported on iOS yet
+  connectionTimeout: options.connectionTimeout, // only supported on Android yet
+  readTimeout: options.readTimeout,    // supported on Android and iOS
+  backgroundTimeout: options.backgroundTimeout,// Maximum time (in milliseconds) to download an entire resource (iOS only, useful for timing out background downloads)
+});
+
 }
 
 export const stopDownload = (jobId: number) => {
