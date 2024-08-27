2b87f1cb8d2f9aba5cb294e8dc13ba09c5fbcca0
滑稽world <65672655+huajiworld@users.noreply.github.com>
2024-08-27 19:46:27 +0800

    Update fs.ts

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -66,17 +66,17 @@ export const downloadFile = (url: string, path: string, options: Omit<RNFS.Downl
     fromUrl: url, // URL to download file from
     toFile: path, // Local filesystem path to save the file to...options,
     headers: options.headers, // An object of headers to be passed to the server
-    background?: boolean;    // Continue the download in the background after the app terminates (iOS only)
-    discretionary?: boolean;  // Allow the OS to control the timing and speed of the download to improve perceived performance  (iOS only)
-    cacheable?: boolean;      // Whether the download can be stored in the shared NSURLCache (iOS only, defaults to true)
+    background?: boolean,    // Continue the download in the background after the app terminates (iOS only)
+    discretionary?: boolean,  // Allow the OS to control the timing and speed of the download to improve perceived performance  (iOS only)
+    cacheable?: boolean,      // Whether the download can be stored in the shared NSURLCache (iOS only, defaults to true)
     progressInterval: options.progressInterval,
     progressDivider: options.progressDivider,
-    begin: (res: DownloadBeginCallbackResult) => void;
-    progress?: (res: DownloadProgressCallbackResult) => void;
-    resumable?: () => void;    // only supported on iOS yet
-    connectionTimeout?: number; // only supported on Android yet
-    readTimeout?: number;    // supported on Android and iOS
-    backgroundTimeout?: number;// Maximum time (in milliseconds) to download an entire resource (iOS only, useful for timing out background downloads)
+    begin: (res: DownloadBeginCallbackResult) => void,
+    progress?: (res: DownloadProgressCallbackResult) => void,
+    resumable?: () => void,    // only supported on iOS yet
+    connectionTimeout?: number, // only supported on Android yet
+    readTimeout?: number,    // supported on Android and iOS
+    backgroundTimeout?: number,// Maximum time (in milliseconds) to download an entire resource (iOS only, useful for timing out background downloads)
   })
 }
 
